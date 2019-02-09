5687838299c937c5fbe2c29e1a3cc058407ed826
Barnabass <rastafarimood@gmail.com>
2019-02-09 15:35:24 +0200

    src/main/java/com/softserve/library/app/dao/implementation/PublisherDaoImpl.java ref

--- src/main/java/com/softserve/library/app/dao/implementation/PublisherDaoImpl.java
+++ src/main/java/com/softserve/library/app/dao/implementation/PublisherDaoImpl.java
@@ -1,28 +1,28 @@
 package com.softserve.library.app.dao.implementation;
 
 import com.softserve.library.app.dao.interfaces.PublisherDao;
+import com.softserve.library.app.dao.statement.PublisherStatementExecutor;
 import com.softserve.library.app.model.Publisher;
 import java.sql.SQLException;
 
 public class PublisherDaoImpl implements PublisherDao {
 
-    @Override
-    public Publisher get(int id) throws SQLException {
-        return null;
-    }
+    private final PublisherStatementExecutor publisherStatementExecutor = new PublisherStatementExecutor();
 
-    @Override
-    public boolean add(Publisher publisher) throws SQLException {
-        return false;
-    }
+    @Override public Publisher get(int id) throws SQLException {
 
-    @Override
-    public boolean delete(int id) throws SQLException {
-        return false;
+        return publisherStatementExecutor.get(id);
     }
+    @Override public boolean add(Publisher publisher) throws SQLException {
 
-    @Override
-    public boolean update(Publisher publisher) throws SQLException {
-        return false;
+        return publisherStatementExecutor.add(publisher);
+    }
+    @Override public boolean delete(int id) throws SQLException {
+
+        return publisherStatementExecutor.delete(id);
+    }
+    @Override public boolean update(Publisher publisher) throws SQLException {
+
+        return publisherStatementExecutor.update(publisher);
     }
 }
