ff537f54522818970bc91d4bbf214d39726ab934
Barnabass <rastafarimood@gmail.com>
2019-02-09 14:54:40 +0200

    src/main/java/com/softserve/library/app/Main.java ref

--- src/main/java/com/softserve/library/app/Main.java
+++ src/main/java/com/softserve/library/app/Main.java
@@ -5,12 +5,20 @@ import com.softserve.library.app.model.Author;
 import com.softserve.library.app.service.interfaces.AuthorService;
 import com.softserve.library.app.service.implementation.AuthorServiceImpl;
 
+import java.sql.SQLException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         AuthorService authorService = new AuthorServiceImpl();
-        
+        Author author;
+        author = authorService.get(14);
+
+
+
+
+
 
         DBConnectivity.closeConnection();
     }
