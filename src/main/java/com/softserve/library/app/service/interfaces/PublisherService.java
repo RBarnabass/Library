cb0ed59bfdf70eb0c846f67d1c35fbc7d1a0d9eb
Barnabass <rastafarimood@gmail.com>
2019-02-09 15:37:28 +0200

    src/main/java/com/softserve/library/app/service/interfaces/PublisherService.java new one

---
+++ src/main/java/com/softserve/library/app/service/interfaces/PublisherService.java
@@ -0,0 +1,7 @@
+package com.softserve.library.app.service.interfaces;
+
+import com.softserve.library.app.model.Publisher;
+
+public interface PublisherService extends SimpleCrudService<Publisher> {
+
+}
