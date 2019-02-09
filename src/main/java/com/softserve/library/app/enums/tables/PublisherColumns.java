b8287a59ac64e3867a822bc636e8097a32b09804
Barnabass <rastafarimood@gmail.com>
2019-02-09 15:11:31 +0200

    src/main/java/com/softserve/library/app/enums/tables/PublisherColumns.java ref

---
+++ src/main/java/com/softserve/library/app/enums/tables/PublisherColumns.java
@@ -0,0 +1,19 @@
+package com.softserve.library.app.enums.tables;
+
+public enum PublisherColumns {
+
+    ID("id"),
+    NAME("name");
+    
+    private String line;
+
+    PublisherColumns(String line) {
+
+        this.line = line;
+    }
+
+    public String getColumn() {
+
+        return line;
+    }
+}
