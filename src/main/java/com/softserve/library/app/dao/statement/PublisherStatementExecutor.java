55fe767911e2bb59cf1b9c75432b3b939d4f6bf8
Barnabass <rastafarimood@gmail.com>
2019-02-09 15:36:21 +0200

    src/main/java/com/softserve/library/app/dao/statement/PublisherStatementExecutor.java new one

---
+++ src/main/java/com/softserve/library/app/dao/statement/PublisherStatementExecutor.java
@@ -0,0 +1,76 @@
+package com.softserve.library.app.dao.statement;
+
+import com.softserve.library.app.config.DBConnectivity;
+import com.softserve.library.app.enums.sql.AuthorSQL;
+import com.softserve.library.app.enums.sql.PublisherSQL;
+import com.softserve.library.app.enums.tables.AuthorColumns;
+import com.softserve.library.app.enums.tables.PublisherColumns;
+import com.softserve.library.app.model.Author;
+import com.softserve.library.app.model.Publisher;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PublisherStatementExecutor {
+
+    private boolean isSuccess;
+
+    // todo: the same as the author executor !!!
+    public Publisher get(int id) throws SQLException {
+
+        List<Publisher> list = new ArrayList<>();
+
+        PreparedStatement preparedStatement = DBConnectivity.getConnection().prepareStatement(PublisherSQL.SELECT.getSQL() + scopesWrapper(id));
+        ResultSet set = preparedStatement.executeQuery();
+        Publisher publisher;
+
+        while (set.next()) {
+
+            publisher = new Publisher();
+            publisher.setId(set.getInt(PublisherColumns.ID.getColumn()));
+            publisher.setName(set.getString(PublisherColumns.NAME.getColumn()));
+            list.add(publisher);
+        }
+
+        set.close();
+        preparedStatement.close();
+
+        return list.get(0);
+    }
+    public boolean add(Publisher publisher) throws SQLException {
+
+        PreparedStatement preparedStatement = DBConnectivity.getConnection().prepareStatement(PublisherSQL.INSERT.getSQL());
+        preparedStatement.setString(1, publisher.getName());
+        isSuccess = preparedStatement.executeUpdate() > 0;
+        preparedStatement.close();
+
+        return isSuccess;
+    }
+    public boolean delete(int id) throws SQLException {
+
+        Statement statement = DBConnectivity.getConnection().createStatement();
+        isSuccess = statement.executeUpdate(PublisherSQL.DELETE.getSQL() + scopesWrapper(id)) > 0;
+        statement.close();
+
+        return isSuccess;
+    }
+    public boolean update(Publisher publisher) throws SQLException {
+
+        PreparedStatement preparedStatement = DBConnectivity.getConnection().prepareStatement(PublisherSQL.UPDATE.getSQL());
+        preparedStatement.setString(1, publisher.getName());
+        preparedStatement.setInt(2, publisher.getId());
+        isSuccess = preparedStatement.executeUpdate() > 0;
+        preparedStatement.close();
+
+        return isSuccess;
+    }
+
+    private String scopesWrapper(int id) {
+
+        return "'" + id + "'";
+    }
+}
