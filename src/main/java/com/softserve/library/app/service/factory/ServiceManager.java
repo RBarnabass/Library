96709662926f9e039178742f328870c18c47592f
Barnabass <rastafarimood@gmail.com>
2019-02-10 10:09:41 +0200

    authorize

--- src/main/java/com/softserve/library/app/service/factory/ServiceManager.java
+++ src/main/java/com/softserve/library/app/service/factory/ServiceManager.java
@@ -2,6 +2,11 @@ package com.softserve.library.app.service.factory;
 
 import com.softserve.library.app.service.interfaces.SimpleCrudService;
 
+/**
+ *
+ *
+ * @author Roman Berezhnov
+ */
 public interface ServiceManager {
 
     SimpleCrudService getService(String name);
