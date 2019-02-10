bff395186f816280b73bd85b6301e0a7e27db7af
Barnabass <rastafarimood@gmail.com>
2019-02-10 10:12:24 +0200

    authorize

--- src/main/java/com/softserve/library/app/service/interfaces/PublisherService.java
+++ src/main/java/com/softserve/library/app/service/interfaces/PublisherService.java
@@ -2,6 +2,11 @@ package com.softserve.library.app.service.interfaces;
 
 import com.softserve.library.app.model.Publisher;
 
+/**
+ *
+ *
+ * @author Roman Berezhnov
+ */
 public interface PublisherService extends SimpleCrudService<Publisher> {
 
 }
