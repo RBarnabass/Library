400046a1c320e7a303e0e9bdcb80e62130d658bf
Barnabass <rastafarimood@gmail.com>
2019-02-10 09:44:55 +0200

    testing new factory implementation

--- src/main/java/com/softserve/library/app/Main.java
+++ src/main/java/com/softserve/library/app/Main.java
@@ -1,14 +1,10 @@
 package com.softserve.library.app;
 
 import com.softserve.library.app.config.DBConnectivity;
-import com.softserve.library.app.dao.implementation.BookDaoImpl;
-import com.softserve.library.app.dao.interfaces.BookDao;
 import com.softserve.library.app.model.Author;
-import com.softserve.library.app.model.Book;
-import com.softserve.library.app.service.implementation.BookServiceImpl;
+import com.softserve.library.app.service.factory.ServiceFactory;
+import com.softserve.library.app.service.factory.ServiceFactoryImpl;
 import com.softserve.library.app.service.interfaces.AuthorService;
-import com.softserve.library.app.service.implementation.AuthorServiceImpl;
-import com.softserve.library.app.service.interfaces.BookService;
 
 import java.sql.SQLException;
 
@@ -16,13 +12,11 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
 
-        BookService bookService = new BookServiceImpl();
-        Book book = bookService.get(5);
-        System.out.println(book);
+        Author author = new Author("Factory");
 
-
-
-
+        ServiceFactory serviceFactory = new ServiceFactoryImpl();
+        AuthorService authorService = serviceFactory.getAuthorService();
+        authorService.add(author);
 
         DBConnectivity.closeConnection();
     }
