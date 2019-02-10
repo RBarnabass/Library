46263134737284ba17fecf8b4d7875ab2e5af6d6
Barnabass <rastafarimood@gmail.com>
2019-02-10 10:11:29 +0200

    authorize

--- src/main/java/com/softserve/library/app/service/interfaces/AuthorService.java
+++ src/main/java/com/softserve/library/app/service/interfaces/AuthorService.java
@@ -2,6 +2,11 @@ package com.softserve.library.app.service.interfaces;
 
 import com.softserve.library.app.model.Author;
 
+/**
+ *
+ *
+ * @author Roman Berezhnov
+ */
 public interface AuthorService extends SimpleCrudService<Author> {
 
 }
