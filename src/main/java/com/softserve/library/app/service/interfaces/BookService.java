d645086928a6e8ad7924df6d1986db5b438c4752
Barnabass <rastafarimood@gmail.com>
2019-02-10 10:12:02 +0200

    authorize

--- src/main/java/com/softserve/library/app/service/interfaces/BookService.java
+++ src/main/java/com/softserve/library/app/service/interfaces/BookService.java
@@ -2,6 +2,11 @@ package com.softserve.library.app.service.interfaces;
 
 import com.softserve.library.app.model.Book;
 
+/**
+ *
+ *
+ * @author Roman Berezhnov
+ */
 public interface BookService extends SimpleCrudService<Book> {
 
 }
