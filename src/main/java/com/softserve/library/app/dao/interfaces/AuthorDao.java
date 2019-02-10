96962c04d1ed614cd8f44c2fb4bdaceda5e43233
Barnabass <rastafarimood@gmail.com>
2019-02-10 10:00:11 +0200

    authorize

--- src/main/java/com/softserve/library/app/dao/interfaces/AuthorDao.java
+++ src/main/java/com/softserve/library/app/dao/interfaces/AuthorDao.java
@@ -2,6 +2,11 @@ package com.softserve.library.app.dao.interfaces;
 
 import com.softserve.library.app.model.Author;
 
+/**
+ *
+ *
+ * @author Roman Berezhnov
+ */
 public interface AuthorDao extends SimpleCrudDao<Author> {
 
 }
