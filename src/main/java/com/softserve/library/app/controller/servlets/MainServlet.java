f4c740bc5e09bae333d5b152769fa83bad8f93ae
Barnabass <rastafarimood@gmail.com>
2019-02-10 09:45:36 +0200

    testing main servlet attitude

--- src/main/java/com/softserve/library/app/controller/servlets/MainServlet.java
+++ src/main/java/com/softserve/library/app/controller/servlets/MainServlet.java
@@ -1,15 +1,31 @@
 package com.softserve.library.app.controller.servlets;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name = "MainServlet", urlPatterns = "/library")
 public class MainServlet extends HttpServlet {
 
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        DispatcherType dispatcherType = req.getDispatcherType();
+        System.out.println("Disp type - " + dispatcherType);
+        System.out.println("Session - " + req.getSession());
+        System.out.println("Auth - " + req.authenticate(resp));
+        System.out.println("Auth type - " + req.getAuthType());
+        System.out.println("req session id -" + req.getRequestedSessionId());
+        RequestDispatcher view = req.getRequestDispatcher("main.jsp");
+        view.forward(req, resp);
 
-
+    }
 
     @Override public void init() {
         System.out.println(" - - - Main servlet was initialized !");
