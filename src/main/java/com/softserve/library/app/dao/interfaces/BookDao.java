588dd6f258c39d377bf04af72142f5e07f1d1cf6
Barnabass <rastafarimood@gmail.com>
2019-02-10 10:00:32 +0200

    authorize

--- src/main/java/com/softserve/library/app/dao/interfaces/BookDao.java
+++ src/main/java/com/softserve/library/app/dao/interfaces/BookDao.java
@@ -2,6 +2,11 @@ package com.softserve.library.app.dao.interfaces;
 
 import com.softserve.library.app.model.Book;
 
+/**
+ *
+ *
+ * @author Roman Berezhnov
+ */
 public interface BookDao extends SimpleCrudDao<Book> {
 
 }
