f8075efe006503816b36d90cefb7aea1ade8b178
Barnabass <rastafarimood@gmail.com>
2019-02-10 10:00:57 +0200

    authorize

--- src/main/java/com/softserve/library/app/dao/interfaces/PublisherDao.java
+++ src/main/java/com/softserve/library/app/dao/interfaces/PublisherDao.java
@@ -2,6 +2,11 @@ package com.softserve.library.app.dao.interfaces;
 
 import com.softserve.library.app.model.Publisher;
 
+/**
+ *
+ *
+ * @author Roman Berezhnov
+ */
 public interface PublisherDao extends SimpleCrudDao<Publisher> {
 
 }
