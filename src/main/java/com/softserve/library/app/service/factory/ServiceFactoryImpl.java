94eee153fa8f861cbae01a8635820d06ac2a6f07
Barnabass <rastafarimood@gmail.com>
2019-02-11 11:51:43 +0200

    realize improved factory

--- src/main/java/com/softserve/library/app/service/factory/ServiceFactoryImpl.java
+++ src/main/java/com/softserve/library/app/service/factory/ServiceFactoryImpl.java
@@ -3,6 +3,7 @@ package com.softserve.library.app.service.factory;
 import com.softserve.library.app.service.interfaces.AuthorService;
 import com.softserve.library.app.service.interfaces.BookService;
 import com.softserve.library.app.service.interfaces.PublisherService;
+import com.softserve.library.app.service.interfaces.UserService;
 
 /**
  *
@@ -13,21 +14,21 @@ public class ServiceFactoryImpl implements ServiceFactory {
 
     private final ServiceManager serviceManager = new ServiceContainer();
 
-    @Override
-    public AuthorService getAuthorService() {
+    // todo: change literals to enum !
+    @Override public AuthorService getAuthorService() {
 
         return (AuthorService) serviceManager.getService("author");
     }
-
-    @Override
-    public BookService getBookService() {
+    @Override public BookService getBookService() {
 
         return (BookService) serviceManager.getService("book");
     }
-
-    @Override
-    public PublisherService getPublisherService() {
+    @Override public PublisherService getPublisherService() {
 
         return (PublisherService) serviceManager.getService("publisher");
+    }
+    @Override public UserService getUserService() {
+
+        return (UserService) serviceManager.getService("user");
     }
 }
