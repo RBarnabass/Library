5c2e556f6fea808a02f3cffe8434ab18d0c42195
Barnabass <rastafarimood@gmail.com>
2019-02-11 13:55:46 +0200

    impl credential statement

--- src/main/java/com/softserve/library/app/dao/statement/CredentialStatementExecutor.java
+++ src/main/java/com/softserve/library/app/dao/statement/CredentialStatementExecutor.java
@@ -3,8 +3,11 @@ package com.softserve.library.app.dao.statement;
 import com.softserve.library.app.config.DBConnectivity;
 import com.softserve.library.app.enums.sql.CredentialSQL;
 import com.softserve.library.app.enums.tables.CredentialColumns;
+import com.softserve.library.app.enums.tables.RoleColumns;
 import com.softserve.library.app.model.Author;
 import com.softserve.library.app.model.Credential;
+import com.softserve.library.app.model.Role;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -24,9 +27,12 @@ public class CredentialStatementExecutor {
 
         List<Credential> list = new ArrayList<>();
 
-        PreparedStatement preparedStatement = DBConnectivity.getConnection().prepareStatement(CredentialSQL.SELECT.getSQL() + login);
+        //todo: wrapper method !
+        PreparedStatement preparedStatement = DBConnectivity.getConnection().prepareStatement(CredentialSQL.SELECT.getSQL() + "'" + login + "'");
+        preparedStatement.executeQuery();
         ResultSet set = preparedStatement.getResultSet();
         Credential credential;
+        Role role;
 
         while (set.next()) {
 
@@ -34,6 +40,11 @@ public class CredentialStatementExecutor {
             credential.setId(set.getInt(CredentialColumns.ID.getColumn()));
             credential.setLogin(set.getString(CredentialColumns.LOGIN.getColumn()));
             credential.setPassword(set.getString(CredentialColumns.PASSWORD.getColumn()));
+
+            role = new Role();
+            role.setId(set.getInt(RoleColumns.ID.getColumn()));
+            role.setRole(set.getString(RoleColumns.TYPE.getColumn()));
+            credential.setRole(role);
 
             list.add(credential);
         }
@@ -46,7 +57,7 @@ public class CredentialStatementExecutor {
 
     public boolean add(Credential credential) {
 
-        
+
 
 
 
