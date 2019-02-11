88f3c5091a4c480abdbb8774a860781ffc80ed0b
Barnabass <rastafarimood@gmail.com>
2019-02-11 11:50:18 +0200

    add new service

--- src/main/java/com/softserve/library/app/service/factory/ServiceContainer.java
+++ src/main/java/com/softserve/library/app/service/factory/ServiceContainer.java
@@ -3,6 +3,7 @@ package com.softserve.library.app.service.factory;
 import com.softserve.library.app.service.implementation.AuthorServiceImpl;
 import com.softserve.library.app.service.implementation.BookServiceImpl;
 import com.softserve.library.app.service.implementation.PublisherServiceImpl;
+import com.softserve.library.app.service.implementation.UserServiceImpl;
 import com.softserve.library.app.service.interfaces.SimpleCrudService;
 import java.util.HashMap;
 import java.util.Map;
@@ -43,6 +44,10 @@ public class ServiceContainer implements ServiceManager {
             }
             case "publisher": {
                 service = new PublisherServiceImpl();
+                break;
+            }
+            case "user": {
+                service = new UserServiceImpl();
                 break;
             }
         }
