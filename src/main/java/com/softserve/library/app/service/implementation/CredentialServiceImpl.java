237bf7b23ed8aec82dfd15e1f74476f1717a8944
Barnabass <rastafarimood@gmail.com>
2019-02-11 13:25:04 +0200

    added credential service impl

---
+++ src/main/java/com/softserve/library/app/service/implementation/CredentialServiceImpl.java
@@ -0,0 +1,43 @@
+package com.softserve.library.app.service.implementation;
+
+import com.softserve.library.app.dao.implementation.CredentialDaoImpl;
+import com.softserve.library.app.dao.interfaces.CredentialDao;
+import com.softserve.library.app.model.Credential;
+import com.softserve.library.app.service.interfaces.CredentialService;
+import java.sql.SQLException;
+
+/**
+ *
+ *
+ * @author Roman Berezhnov
+ */
+public class CredentialServiceImpl implements CredentialService {
+
+    private final CredentialDao credentialDao = new CredentialDaoImpl();
+
+    @Override public Credential getByLogin(String login) throws SQLException {
+
+        return credentialDao.getByLogin(login);
+    }
+
+    @Override
+    public Credential get(int id) throws SQLException {
+
+        return null;
+    }
+
+    @Override
+    public boolean add(Credential credential) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(int id) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean update(Credential credential) throws SQLException {
+        return false;
+    }
+}
