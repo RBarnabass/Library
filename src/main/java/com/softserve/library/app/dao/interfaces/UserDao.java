daa7891ebd4522ec9cf5cc9734025ab3b263387b
Barnabass <rastafarimood@gmail.com>
2019-02-11 11:53:35 +0200

    create user dao interface

---
+++ src/main/java/com/softserve/library/app/dao/interfaces/UserDao.java
@@ -0,0 +1,12 @@
+package com.softserve.library.app.dao.interfaces;
+
+import com.softserve.library.app.model.User;
+
+/**
+ *
+ *
+ * @author Roman Berezhnov
+ */
+public interface UserDao extends SimpleCrudDao<User> {
+
+}
