9901f35e62fc7c6a6c1ad856f9a52ee9c22d5248
Barnabass <rastafarimood@gmail.com>
2019-02-11 13:20:22 +0200

    new entity

--- src/main/java/com/softserve/library/app/model/Credential.java
+++ src/main/java/com/softserve/library/app/model/Credential.java
@@ -9,50 +9,55 @@ import java.util.Objects;
  */
 public class Credential {
 
+    private int id;
     private String login;
     private String password;
 
     public Credential() { }
-
     public Credential(String login, String password) {
         this.login = login;
         this.password = password;
     }
+    public Credential(int id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
 
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
     public String getLogin() {
         return login;
     }
-
     public void setLogin(String login) {
         this.login = login;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
 
-    @Override
-    public boolean equals(Object o) {
+    @Override public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Credential that = (Credential) o;
-        return Objects.equals(login, that.login) &&
+        return id == that.id &&
+                Objects.equals(login, that.login) &&
                 Objects.equals(password, that.password);
     }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(login, password);
+    @Override public int hashCode() {
+        return Objects.hash(id, login, password);
     }
-
-    @Override
-    public String toString() {
+    @Override public String toString() {
         return "Credential{" +
-                "login='" + login + '\'' +
+                "id=" + id +
+                ", login='" + login + '\'' +
                 ", password='" + password + '\'' +
                 '}';
     }
