709eff6d77aaf4ce47996f66a35e36f5867c900d
Barnabass <rastafarimood@gmail.com>
2019-02-11 15:47:10 +0200

    add new welcome servlet for welcome page

---
+++ src/main/java/com/softserve/library/app/controller/servlets/HeyServlet.java
@@ -0,0 +1,23 @@
+package com.softserve.library.app.controller.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ *
+ * @author Roman Berezhnov
+ */
+@WebServlet(name = "hey", urlPatterns = "/welcome")
+public class HeyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("-------------------------------------Hey servlet------------------------------------");
+        req.getRequestDispatcher("/view/welcome.jsp").forward(req, resp);
+    }
+}
