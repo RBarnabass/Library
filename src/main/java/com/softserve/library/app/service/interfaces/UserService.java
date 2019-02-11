5d23031d37a0201ef7a9c13075856475351d46dd
Barnabass <rastafarimood@gmail.com>
2019-02-11 12:00:45 +0200

    added new interface for user service

---
+++ src/main/java/com/softserve/library/app/service/interfaces/UserService.java
@@ -0,0 +1,12 @@
+package com.softserve.library.app.service.interfaces;
+
+import com.softserve.library.app.model.User;
+
+/**
+ *
+ *
+ * @author Roman Berezhnov
+ */
+public interface UserService extends SimpleCrudService<User> {
+
+}
