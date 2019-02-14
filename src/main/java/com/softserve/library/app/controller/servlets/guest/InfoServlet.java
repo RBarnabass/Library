94634e75421beb70f570c1bd7a3ea7d52b5dbd34
Barnabass <rastafarimood@gmail.com>
2019-02-14 17:30:05 +0200

    refactoring

--- src/main/java/com/softserve/library/app/controller/servlets/guest/InfoServlet.java
+++ src/main/java/com/softserve/library/app/controller/servlets/guest/InfoServlet.java
@@ -8,6 +8,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ *
+ *
+ * @author Roman Berezhnov
+ */
 @WebServlet({"/library", "/library/info"})
 public class InfoServlet extends HttpServlet {
 
