5e2bd627c2ce52e2b3135460c6263d37b5ff42ab
Barnabass <rastafarimood@gmail.com>
2019-02-14 19:37:32 +0200

    implement book list

--- src/main/java/com/softserve/library/app/controller/servlets/user/BookListServlet.java
+++ src/main/java/com/softserve/library/app/controller/servlets/user/BookListServlet.java
@@ -1,5 +1,9 @@
 package com.softserve.library.app.controller.servlets.user;
 
+import com.softserve.library.app.dto.BookDto;
+import com.softserve.library.app.service.factory.ServiceFactory;
+import com.softserve.library.app.service.factory.ServiceFactoryImpl;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,6 +11,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 /**
  *
@@ -18,6 +24,15 @@ public class BookListServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        ServiceFactory serviceFactory = new ServiceFactoryImpl();
+        List<BookDto> all = null;
+        try {
+            all = serviceFactory.getBookService().getAll();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        req.getSession().setAttribute("bookList", all);
 
         RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/view/user/book_list.jsp");
         dispatcher.forward(req, resp);
