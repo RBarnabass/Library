5026f826ec41fdef62a32d72fada3db30bf91daa
Barnabass <rastafarimood@gmail.com>
2019-02-14 19:39:29 +0200

    new service

--- src/main/java/com/softserve/library/app/service/implementation/BookServiceImpl.java
+++ src/main/java/com/softserve/library/app/service/implementation/BookServiceImpl.java
@@ -2,9 +2,11 @@ package com.softserve.library.app.service.implementation;
 
 import com.softserve.library.app.dao.implementation.BookDaoImpl;
 import com.softserve.library.app.dao.interfaces.BookDao;
+import com.softserve.library.app.dto.BookDto;
 import com.softserve.library.app.model.Book;
 import com.softserve.library.app.service.interfaces.BookService;
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  *
@@ -14,11 +16,6 @@ import java.sql.SQLException;
 public class BookServiceImpl implements BookService {
 
     private final BookDao bookDao = new BookDaoImpl();
-
-    @Override public Book get(int id) throws SQLException {
-
-        return bookDao.get(id);
-    }
 
     @Override
     public boolean add(Book book) throws SQLException {
@@ -33,5 +30,18 @@ public class BookServiceImpl implements BookService {
     @Override
     public boolean update(Book book) throws SQLException {
         return false;
+    }
+
+    @Override public Book get(int id) throws SQLException {
+
+        return bookDao.get(id);
+    }
+    @Override public List<BookDto> getAll() throws SQLException {
+
+        return bookDao.getAll();
+    }
+    @Override public List<Boolean> getAllAvailable() throws SQLException {
+
+        return bookDao.getAllAvailable();
     }
 }
