df633e23fbe6b70a3546ac3d18be734b22c75e38
Barnabass <rastafarimood@gmail.com>
2019-02-14 17:07:32 +0200

    correct way to jsp page

--- src/main/java/com/softserve/library/app/controller/servlets/user/BookOrderServlet.java
+++ src/main/java/com/softserve/library/app/controller/servlets/user/BookOrderServlet.java
@@ -14,7 +14,7 @@ public class BookOrderServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/viewTest/book_order.jsp");
+        RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/view/user/book_order.jsp");
         dispatcher.forward(req, resp);
     }
 }
