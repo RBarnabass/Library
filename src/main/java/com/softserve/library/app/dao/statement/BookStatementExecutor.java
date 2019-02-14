5d5bbba787bf7b18f8025ac9427e7c86ba08cd95
Barnabass <rastafarimood@gmail.com>
2019-02-14 19:38:59 +0200

    new methods

--- src/main/java/com/softserve/library/app/dao/statement/BookStatementExecutor.java
+++ src/main/java/com/softserve/library/app/dao/statement/BookStatementExecutor.java
@@ -1,9 +1,12 @@
 package com.softserve.library.app.dao.statement;
 
 import com.softserve.library.app.config.DBConnectivity;
+import com.softserve.library.app.dto.BookDto;
 import com.softserve.library.app.enums.sql.BookSQL;
+import com.softserve.library.app.enums.tables.AuthorColumns;
 import com.softserve.library.app.enums.tables.BookColumns;
 import com.softserve.library.app.enums.tables.PublisherColumns;
+import com.softserve.library.app.model.Author;
 import com.softserve.library.app.model.Book;
 import com.softserve.library.app.model.Publisher;
 
@@ -22,7 +25,7 @@ public class BookStatementExecutor {
 
     private boolean isSuccess;
 
-    public Book get(int id) throws SQLException {
+    public List<Book> get(int id) throws SQLException {
 
         List<Book> list = new ArrayList<>();
 
@@ -48,7 +51,64 @@ public class BookStatementExecutor {
             list.add(book);
         }
 
-        return list.get(0);
+        return list;
+    }
+    public List<BookDto> getAll() throws SQLException {
+
+        List<BookDto> list = new ArrayList<>();
+
+        String sql = "SELECT book.id, book.name, book.publish_year, publisher.name, author.full_name FROM book\n" +
+                "  JOIN publisher\n" +
+                "    ON book.publisher_id = publisher.id\n" +
+                "      JOIN book_by_authors\n" +
+                "        ON book.id = book_by_authors.book_id\n" +
+                "          JOIN author\n" +
+                "            ON book_by_authors.author_id = author.id\n" +
+                "              WHERE book_by_authors.is_primary = true";
+
+        PreparedStatement preparedStatement = DBConnectivity.getConnection().prepareStatement(sql);
+        preparedStatement.executeQuery();
+        ResultSet set = preparedStatement.getResultSet();
+
+        BookDto bookDto;
+
+        while (set.next()) {
+
+            bookDto = new BookDto();
+            bookDto.setId(set.getInt(BookColumns.ID.getColumn()));
+            bookDto.setBookName(set.getString(BookColumns.NAME.getColumn()));
+            bookDto.setPublishYear(set.getInt(BookColumns.PUBLISH_YEAR.getColumn()));
+
+            bookDto.setPublisherName(set.getString(PublisherColumns.NAME.getColumn()));
+
+            bookDto.setPrimaryAuthor(set.getString(AuthorColumns.FULL_NAME.getColumn()));
+
+            list.add(bookDto);
+        }
+
+        return list;
+
+    }
+
+    public List<Boolean> getAllAvailable() throws SQLException {
+
+        List<Boolean> list = new ArrayList<>();
+
+        String sql = "SELECT copy.is_available FROM copy\n" +
+                "  JOIN book\n" +
+                "    ON copy.book_id = book.id\n" +
+                "      WHERE book_id = 5 and is_available = true;";
+
+        PreparedStatement preparedStatement = DBConnectivity.getConnection().prepareStatement(sql);
+        preparedStatement.executeQuery();
+        ResultSet set = preparedStatement.getResultSet();
+
+        while (set.next()) {
+
+            list.add(set.getBoolean("copy.is_available"));
+        }
+
+        return list;
     }
 
     private String scopesWrapper(int id) {
