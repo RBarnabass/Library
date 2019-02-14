04d89ea7a86a0ea6da3e894cd0b81f9a9d927df5
Barnabass <rastafarimood@gmail.com>
2019-02-14 19:37:59 +0200

    new method

--- src/main/java/com/softserve/library/app/dao/implementation/BookDaoImpl.java
+++ src/main/java/com/softserve/library/app/dao/implementation/BookDaoImpl.java
@@ -2,8 +2,10 @@ package com.softserve.library.app.dao.implementation;
 
 import com.softserve.library.app.dao.interfaces.BookDao;
 import com.softserve.library.app.dao.statement.BookStatementExecutor;
+import com.softserve.library.app.dto.BookDto;
 import com.softserve.library.app.model.Book;
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  *
@@ -16,7 +18,17 @@ public class BookDaoImpl implements BookDao {
 
     @Override public Book get(int id) throws SQLException {
 
-        return bookStatementExecutor.get(id);
+        List<Book> list = bookStatementExecutor.get(id);
+
+        return list.size() > 1 ? null : list.get(0);
+    }
+    @Override public List<BookDto> getAll() throws SQLException {
+
+        return bookStatementExecutor.getAll();
+    }
+    @Override public List<Boolean> getAllAvailable() throws SQLException {
+
+        return bookStatementExecutor.getAllAvailable();
     }
 
     @Override
