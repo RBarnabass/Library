e08d34c66da5652ab95b9bad7f8ad706d6249015
Barnabass <rastafarimood@gmail.com>
2019-02-15 09:56:37 +0200

    implement method add in credential chain

--- src/main/java/com/softserve/library/app/service/implementation/CredentialServiceImpl.java
+++ src/main/java/com/softserve/library/app/service/implementation/CredentialServiceImpl.java
@@ -3,6 +3,7 @@ package com.softserve.library.app.service.implementation;
 import com.softserve.library.app.dao.implementation.CredentialDaoImpl;
 import com.softserve.library.app.dao.interfaces.CredentialDao;
 import com.softserve.library.app.model.Credential;
+import com.softserve.library.app.model.Role;
 import com.softserve.library.app.service.interfaces.CredentialService;
 import java.sql.SQLException;
 
@@ -23,19 +24,22 @@ public class CredentialServiceImpl implements CredentialService {
 
         return credentialDao.checkExistence(login);
     }
+    @Override public boolean add(Credential credential) throws SQLException {
+
+        //todo: should ask role in role dao !!!
+
+        Role role = new Role();
+        role.setId(1);
+        credential.setRole(role);
+
+        return credentialDao.add(credential);
+    }
 
     @Override
     public Credential get(int id) throws SQLException {
 
         //todo: implement me !
         return null;
-    }
-
-    @Override
-    public boolean add(Credential credential) throws SQLException {
-
-        //todo: implement me !
-        return false;
     }
 
     @Override
