6f6f183fd2f358a555bf8aa3239ae29553e93f7b
Barnabass <rastafarimood@gmail.com>
2019-02-16 21:56:26 +0200

    refactoring and statements close

--- src/main/java/com/softserve/library/app/dao/statement/UserStatementExecutor.java
+++ src/main/java/com/softserve/library/app/dao/statement/UserStatementExecutor.java
@@ -62,9 +62,12 @@ public class UserStatementExecutor {
         return isSuccess;
     }
 
+    // Task 3 - the usage period is repeated in every line!
     public List<UserStatisticDto> getUserStatistic(int id) throws SQLException {
 
         List<UserStatisticDto> list = new ArrayList<>();
+
+        // todo: remove SQL !!!
 
         String sql = "SELECT user.full_name, from_days(datediff(curdate(), user.registration_date)) AS usingTime, book.name, book.publish_year, time_period.start_date, time_period.end_date\n" +
                 "  FROM user\n" +
@@ -94,6 +97,9 @@ public class UserStatementExecutor {
             list.add(userStatisticDto);
         }
 
+        resultSet.close();
+        preparedStatement.close();
+
         return list;
     }
 
@@ -112,6 +118,9 @@ public class UserStatementExecutor {
 
             avgAge = resultSet.getInt("avgAge");
         }
+
+        resultSet.close();
+        preparedStatement.close();
 
         return avgAge;
     }
@@ -135,6 +144,9 @@ public class UserStatementExecutor {
 
             avgAge = resultSet.getInt("avgAge");
         }
+
+        resultSet.close();
+        preparedStatement.close();
 
         return avgAge;
     }
@@ -161,6 +173,9 @@ public class UserStatementExecutor {
             avgAge = resultSet.getInt("avgAge");
         }
 
+        resultSet.close();
+        preparedStatement.close();
+
         return avgAge;
     }
 
@@ -180,6 +195,9 @@ public class UserStatementExecutor {
 
             daysUsing = resultSet.getInt("daysOfUse");
         }
+
+        resultSet.close();
+        preparedStatement.close();
 
         return daysUsing;
     }
@@ -249,6 +267,9 @@ public class UserStatementExecutor {
 
             debtors.add(debtor);
         }
+
+        resultSet.close();
+        preparedStatement.close();
 
         return debtors;
     }
