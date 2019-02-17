205032a12c0ce7fec7eff3329325629c0a666bf9
Barnabass <rastafarimood@gmail.com>
2019-02-17 23:08:05 +0200

    new security decision

--- src/main/java/com/softserve/library/app/security/SecurityUtils.java
+++ src/main/java/com/softserve/library/app/security/SecurityUtils.java
@@ -1,11 +1,6 @@
 package com.softserve.library.app.security;
 
-import com.softserve.library.app.model.Credential;
 import javax.servlet.http.HttpSession;
-import java.lang.reflect.MalformedParameterizedTypeException;
-import java.sql.Time;
-import java.sql.Timestamp;
-import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -20,35 +15,27 @@ import java.util.concurrent.TimeUnit;
  */
 public class SecurityUtils {
 
-    private static final Map<String, String> sessions = new ConcurrentHashMap<>();
-    private static final int SESSION_LIFETIME_IN_SECONDS = 1800;
+    private static final Map<String, Sess> sessions = new ConcurrentHashMap<>();
+    private static final int SESSION_LIFETIME_IN_SECONDS = 30;
 
-    public static Credential getLoggedUser(HttpSession session) {
+    public static String getLoggedUser(HttpSession session) {
 
         if (sessions.containsKey(session.getId())) {
 
             System.out.println(" - - - Session id is correct !!! - - - ");
-           // return sessions.get(session.getId());
-        } else {
-            System.out.println(" - - - Session id is NOT correct !!! - - - ");
-            //return null;
+            sessions.get(session.getId()).setLastTouch(session.getLastAccessedTime());
+            return sessions.get(session.getId()).getRole();
         }
 
-        return (Credential) session.getAttribute("credential");
+        System.out.println(" - - - Session id is NOT correct !!! - - - ");
+        return null;
     }
 
-    public static void storeLoggedUser(HttpSession session, Credential credential) {
-
-        System.out.println(" - - - User was saved in session ! - - - ");
-
-        // todo: do not hold pass and login in session !!!
-
-        // todo: create new entity and map container (token) for holding session id and role with last activity touch. And check this container every filtering !!!
+    public static void storeLoggedUser(HttpSession session, String role) {
 
         session.setMaxInactiveInterval(SESSION_LIFETIME_IN_SECONDS);
-        sessions.put(session.getId(), credential.getRole().getType());
-
-        session.setAttribute("credential", credential);
+        Sess sess = new Sess(session.getId(), session.getLastAccessedTime(), role);
+        sessions.put(session.getId(), sess);
     }
 
     public static boolean isSecurityPage(String servletPath) {
@@ -71,35 +58,58 @@ public class SecurityUtils {
 
     public static boolean hasPermission(String servletPath, String role) {
 
-        System.out.println(" - - - SecurityUtils - - - ask for permission - - - ");
-
         Set<String> allRoles = SecurityConfig.getAllRoles();
 
         if (allRoles.contains(role)) {
 
             List<String> urlPatternsForRole = SecurityConfig.getUrlPatternsForRole(role);
-
             return urlPatternsForRole.contains(servletPath);
         }
+        return false;
+    }
 
-        /*for (String role : allRoles) {
+    public static void checkSessionLife() {
 
-            if (!request.isUserInRole(role)) {
+        for (String line : sessions.keySet()) {
 
-                System.out.println(" - - - SecurityUtils - - - (hasPermission) - (continue) - " + role);
-                continue;
-            }
+            long time = TimeUnit.MILLISECONDS.toSeconds(new Date().getTime() - sessions.get(line).getLastTouch());
 
-            List<String> urlPatterns = SecurityConfig.getUrlPatternsForRole(role);
-
-            if (urlPatterns != null && urlPatterns.contains(urlPattern)) {
-
-                System.out.println(" - - - SecurityUtils - - - (hasPermission) - (true)");
-                return true;
+            if (time > SESSION_LIFETIME_IN_SECONDS) {
+                sessions.remove(line);
             }
         }
-        System.out.println(" - - - SecurityUtils - - - (hasPermission) - (false)");*/
+    }
 
-        return false;
+    static class Sess {
+
+        private String sessionId;
+        private long lastTouch;
+        private String role;
+
+        public Sess() { }
+        Sess(String sessionId, long lastTouch, String role) {
+            this.sessionId = sessionId;
+            this.lastTouch = lastTouch;
+            this.role = role;
+        }
+
+        public String getSessionId() {
+            return sessionId;
+        }
+        public void setSessionId(String sessionId) {
+            this.sessionId = sessionId;
+        }
+        long getLastTouch() {
+            return lastTouch;
+        }
+        void setLastTouch(long lastTouch) {
+            this.lastTouch = lastTouch;
+        }
+        public String getRole() {
+            return role;
+        }
+        public void setRole(String role) {
+            this.role = role;
+        }
     }
 }
