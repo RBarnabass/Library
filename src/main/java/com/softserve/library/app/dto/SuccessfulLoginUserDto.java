a9cd5e6acb0b043999d7819c11a371bbba70c8c5
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-02-18 14:26:56 +0200

    created SuccessfulLoginUserDto with user id and isAdmin field

---
+++ src/main/java/com/softserve/library/app/dto/SuccessfulLoginUserDto.java
@@ -0,0 +1,23 @@
+package com.softserve.library.app.dto;
+
+public class SuccessfulLoginUserDto {
+
+    private int userId;
+    private boolean isAdmin;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+}
