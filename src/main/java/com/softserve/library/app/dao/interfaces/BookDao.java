00ca315c10163730a77311795db404cb05e6ea81
Barnabass <rastafarimood@gmail.com>
2019-02-20 14:04:06 +0200

    new book dao interface

--- src/main/java/com/softserve/library/app/dao/interfaces/BookDao.java
+++ src/main/java/com/softserve/library/app/dao/interfaces/BookDao.java
@@ -1,12 +1,8 @@
 package com.softserve.library.app.dao.interfaces;
 
-import com.softserve.library.app.dto.BookDto;
-import com.softserve.library.app.dto.CopyDto;
-import com.softserve.library.app.http.CustomResponseEntity;
 import com.softserve.library.app.model.Book;
 
 import java.sql.SQLException;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -14,24 +10,22 @@ import java.util.List;
  */
 public interface BookDao extends SimpleCrudDao<Book> {
 
-    List<BookDto> getAll() throws SQLException;
-
-    int getAllAvailableByBookId(int id) throws SQLException;
-
-    List<BookDto> getAllBooksByAuthor(String authorName) throws SQLException;
-
-    int getBookQuantityPublishedFromYear(int year) throws SQLException;
-
-    int getNumberOfOverallBookUsages(int bookId) throws SQLException;
-
-    int getNumberOfBookUsagesByCopy(int copyId) throws SQLException;
-
-    int getAverageReadingTime(int bookId) throws SQLException;
-
-    // TODO: most popular/unpopular within period
-//    int getMostPopularBookWithinPeriod(Date periodStartDate, Date periodEndDate) throws SQLException;
-
-//    int getMostUnopularBookWithinPeriod(Date periodStartDate, Date periodEndDate) throws SQLException;
-
-    List<CopyDto> getAllCopiesByBookName(String bookName) throws SQLException;
+    List<Book> getAllBooks() throws SQLException;
+    List<Book> getAllByPublisher(String publisher) throws SQLException;
+    List<Book> getAllByPublishYearPeriod(int start, int end) throws SQLException;
+    List<Book> getAllByAuthor(String author) throws SQLException;
+    List<Book> getAllByBookName(String bookName) throws SQLException;
+    List<Book> getAllByBookNameAndPublisher(String bookName, String publisher) throws SQLException;
+    List<Book> getAllByBookNameAndYearPeriod(String bookName, int start, int end) throws SQLException;
+    List<Book> getAllByBookNameAndAuthor(String bookName, String author) throws SQLException;
+    List<Book> getAllByBookNameAndPublisherAndYearPeriod(String bookName, String publisher, int start, int end) throws SQLException;
+    List<Book> getAllByBookNameAndPublisherAndAuthor(String bookName, String publisher, String author) throws SQLException;
+    List<Book> getAllByBookNameAndYearPeriodAndAuthor(String bookName, int start, int end, String author) throws SQLException;
+    List<Book> getAllByBookNameAndPublisherYearPeriodAndAuthor(String bookName, String publisher, int start, int end, String author) throws SQLException;
+    List<Book> getAllByPublisherAndYearPeriod(String publisher, int start, int end) throws SQLException;
+    List<Book> getAllByPublisherAndAuthor(String publisher, String author) throws SQLException;
+    List<Book> getAllByPublisherAndYearPeriodAndAuthor(String publisher, int start, int end, String author) throws SQLException;
+    List<Book> getAllByYearPeriod(int start, int end) throws SQLException;
+    List<Book> getAllByYearPeriodAndAuthor(int start, int end, String author) throws SQLException;
+    int addBook(Book book) throws SQLException;
 }
