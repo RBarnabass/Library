13b68efb3785599d57442dfa9fc50060cf6d0a55
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-02-20 17:58:41 +0200

    implemented checking for validation

--- src/main/webapp/resources/js/signup.js
+++ src/main/webapp/resources/js/signup.js
@@ -6,5 +6,24 @@ signupButton.addEventListener('click', function () {
     let hashedPasswordElement = document.getElementById('data-hashedPassword');
 
     let hashedPassword = sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(password));
+
     hashedPasswordElement.value = hashedPassword;
+
 });
+
+function validate() {
+
+    const nameEl = document.getElementById("nameField");
+    const loginEl = document.getElementById("loginField");
+    const dateEl = document.getElementById("birthDateField");
+    const passwordEl = document.getElementById("passwordField");
+
+
+    if(!validateName(nameEl)) return false;
+    if(!validateLogin(loginEl)) return false;
+    if(!validateDate(dateEl)) return false;
+    return validatePassword(passwordEl);
+
+
+
+}
