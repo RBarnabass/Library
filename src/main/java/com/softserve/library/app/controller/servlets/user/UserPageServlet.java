8f85b2ab2464ac5a243b0f9d437efaea2e6802a6
Barnabass <rastafarimood@gmail.com>
2019-02-20 19:47:18 +0200

    user page servlet added

---
+++ src/main/java/com/softserve/library/app/controller/servlets/user/UserPageServlet.java
@@ -0,0 +1,32 @@
+package com.softserve.library.app.controller.servlets.user;
+
+import com.softserve.library.app.model.User;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+
+@WebServlet("/library/user")
+public class UserPageServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        User user = new User();
+        user.setFullName("Vasia Pupkin");
+        user.setBirthDate(LocalDate.parse("1989-01-01"));
+        user.setLogin("avatar");
+
+
+        req.getSession().setAttribute("user", user);
+
+        final RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/view/general/userPage.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
