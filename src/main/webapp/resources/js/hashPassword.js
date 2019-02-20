bf4195f38ab3a4b93c62ad0fe6f8584994512459
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-02-20 14:05:02 +0200

    client-side hashing logic

---
+++ src/main/webapp/resources/js/hashPassword.js
@@ -0,0 +1,13 @@
+const signupButton = document.getElementById('signupButton');
+
+signupButton.addEventListener('click', function () {
+
+    let password = document.getElementById('passwordBox').value;
+    let hashedPasswordElement = document.getElementById('data-hashedPassword');
+
+    let hashedPassword = sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(password));
+    hashedPasswordElement.value = hashedPassword;
+
+    // alert(hashedPassword);
+    // alert(hashedPasswordElement.value);
+});
