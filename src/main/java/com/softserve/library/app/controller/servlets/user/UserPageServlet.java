1b2969c4f2f453ea968e54bf23b930206cb382ed
Barnabass <rastafarimood@gmail.com>
2019-02-21 00:25:20 +0200

    refactor

--- src/main/java/com/softserve/library/app/controller/servlets/user/UserPageServlet.java
+++ src/main/java/com/softserve/library/app/controller/servlets/user/UserPageServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDate;
@@ -22,25 +23,17 @@ public class UserPageServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        User user = new User();
-        user.setFullName("Vasia Pupkin");
-        user.setBirthDate(LocalDate.parse("1989-01-01"));
-        user.setLogin("avatar");
-
-        ServiceFactory serviceFactory = ServiceFactoryImpl.getFactory();
-        User user1 = null;
-
+        final ServiceFactory serviceFactory = ServiceFactoryImpl.getFactory();
+        final int loggedUserId = SecurityUtils.getLoggedUserId(req.getSession());
+        User user = null;
 
         try {
-            user1 = serviceFactory.getUserService().get(SecurityUtils.getLoggedUserId(req.getSession()));
+            user = serviceFactory.getUserService().get(loggedUserId);
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println(" - - - User page servlet _ get user by id exception ! - - - ");
         }
 
-        System.out.println(user1);
-
-        req.getSession().setAttribute("user", user1);
-
+        req.getSession().setAttribute("user", user);
         final RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/view/general/userPage.jsp");
         dispatcher.forward(req, resp);
     }
