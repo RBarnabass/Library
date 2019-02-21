6ca6ed9f9f533ca941486d3f0341a6c683c6222f
Barnabass <rastafarimood@gmail.com>
2019-02-21 06:56:54 +0200

    implemented add book service method

--- src/main/java/com/softserve/library/app/dao/implementation/PublisherDaoImpl.java
+++ src/main/java/com/softserve/library/app/dao/implementation/PublisherDaoImpl.java
@@ -1,11 +1,15 @@
 package com.softserve.library.app.dao.implementation;
 
+import com.softserve.library.app.config.DBConnectivity;
 import com.softserve.library.app.dao.interfaces.PublisherDao;
 import com.softserve.library.app.dao.statement.PublisherStatementExecutor;
-import com.softserve.library.app.dto.PublisherDto;
-import com.softserve.library.app.http.CustomResponseEntity;
+import com.softserve.library.app.enums.sql.PublisherSQL;
 import com.softserve.library.app.model.Publisher;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 /**
@@ -16,28 +20,22 @@ import java.util.List;
 public class PublisherDaoImpl implements PublisherDao {
 
     private final PublisherStatementExecutor publisherStatementExecutor = new PublisherStatementExecutor();
+    private boolean isSuccess;
 
     @Override public Publisher get(int id) throws SQLException {
 
-        List<Publisher> list = publisherStatementExecutor.get(id);
+        List<Publisher> list = publisherStatementExecutor.getAllByOptions("publisher.id=" + scopesWrapper(id + ""));
 
         return list != null && !list.isEmpty() ? list.get(0) : null;
     }
 
     @Override
-    public boolean add(Publisher t) throws SQLException {
-        return false;
+    public boolean add(Publisher publisher) throws SQLException {
+
+        return publisherStatementExecutor.add(publisher);
     }
 
-//    @Override
-//    public CustomResponseEntity<?> add(PublisherDto publisherDto) throws SQLException {
-//        return null;
-//    }
 
-//    @Override public boolean add(Publisher publisher) throws SQLException {
-//
-//        return publisherStatementExecutor.add(publisher);
-//    }
 
     @Override public boolean delete(int id) throws SQLException {
 
@@ -46,5 +44,34 @@ public class PublisherDaoImpl implements PublisherDao {
     @Override public boolean update(Publisher publisher) throws SQLException {
 
         return publisherStatementExecutor.update(publisher);
+    }
+
+    @Override public Publisher getByName(String name) throws SQLException {
+
+        List<Publisher> list = publisherStatementExecutor.getAllByOptions("publisher.name=" + scopesWrapper(name));
+
+        return list != null && !list.isEmpty() ? list.get(0) : null;
+    }
+
+    @Override
+    public int addAndGetIdBack(Publisher publisher) throws SQLException {
+
+        PreparedStatement preparedStatement = DBConnectivity.getConnection().prepareStatement(PublisherSQL.INSERT.getSQL(), Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, publisher.getName());
+        isSuccess = preparedStatement.executeUpdate() > 0;
+
+        if (isSuccess) {
+
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            resultSet.next();
+            return resultSet.getInt(1);
+        }
+
+        return 0;
+    }
+
+    private String scopesWrapper(String param) {
+
+        return "'" + param + "'";
     }
 }
