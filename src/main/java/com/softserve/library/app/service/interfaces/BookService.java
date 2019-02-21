188c60ca3af7fdd49f99cfab2d892605be344e6d
Barnabass <rastafarimood@gmail.com>
2019-02-21 09:09:22 +0200

    merge

--- src/main/java/com/softserve/library/app/service/interfaces/BookService.java
+++ src/main/java/com/softserve/library/app/service/interfaces/BookService.java
@@ -1,5 +1,6 @@
 package com.softserve.library.app.service.interfaces;
 
+import com.softserve.library.app.dto.BookDto;
 import com.softserve.library.app.dto.BookParametersDto;
 import com.softserve.library.app.model.Book;
 
