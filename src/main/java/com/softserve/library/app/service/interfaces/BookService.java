03a7ecd9c15c23bd353a12202969641a18ce8dac
Barnabass <rastafarimood@gmail.com>
2019-02-21 07:00:12 +0200

    implemented add book service method

--- src/main/java/com/softserve/library/app/service/interfaces/BookService.java
+++ src/main/java/com/softserve/library/app/service/interfaces/BookService.java
@@ -1,12 +1,9 @@
 package com.softserve.library.app.service.interfaces;
 
-import com.softserve.library.app.dto.BookDto;
 import com.softserve.library.app.dto.BookParametersDto;
-import com.softserve.library.app.dto.CopyDto;
 import com.softserve.library.app.model.Book;
 
 import java.sql.SQLException;
-import java.util.Date;
 import java.util.List;
 
 /**
