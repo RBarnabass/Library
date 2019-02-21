a5b6102e0deecdabb46b7ecfff67a3a607fac9ee
Barnabass <rastafarimood@gmail.com>
2019-02-21 06:56:44 +0200

    implemented add book service method

--- src/main/java/com/softserve/library/app/dao/implementation/AuthorDaoImpl.java
+++ src/main/java/com/softserve/library/app/dao/implementation/AuthorDaoImpl.java
@@ -1,12 +1,15 @@
 package com.softserve.library.app.dao.implementation;
 
+import com.softserve.library.app.config.DBConnectivity;
 import com.softserve.library.app.dao.interfaces.AuthorDao;
 import com.softserve.library.app.dao.statement.AuthorStatementExecutor;
-import com.softserve.library.app.dto.AuthorDto;
-import com.softserve.library.app.http.CustomResponseEntity;
+import com.softserve.library.app.enums.sql.AuthorSQL;
 import com.softserve.library.app.model.Author;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 /**
@@ -17,35 +20,55 @@ import java.util.List;
 public class AuthorDaoImpl implements AuthorDao {
 
     private final AuthorStatementExecutor authorStatementExecutor = new AuthorStatementExecutor();
+    private boolean isSuccess;
 
     @Override public Author get(int id) throws SQLException {
 
-        List<Author> list = authorStatementExecutor.get(id);
+        List<Author> list = authorStatementExecutor.getAllByOption(id + "");
 
         return list != null && !list.isEmpty() ? list.get(0) : null;
+    }
+    @Override public Author getByName(String name) throws SQLException {
+
+        List<Author> list = authorStatementExecutor.getAllByOption("author.full_name=" + wrapper(name));
+
+        return list != null && !list.isEmpty() ? list.get(0) : null;
+    }
+
+    @Override
+    public int addAndGetIdBack(Author author) throws SQLException {
+
+        PreparedStatement preparedStatement = DBConnectivity.getConnection().prepareStatement(AuthorSQL.INSERT.getSQL(), Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, author.getName());
+        isSuccess = preparedStatement.executeUpdate() > 0;
+
+        if (isSuccess) {
+
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            resultSet.next();
+            return resultSet.getInt(1);
+        }
+
+        return 0;
     }
 
     @Override
     public boolean add(Author t) throws SQLException {
         return false;
     }
-
-//    @Override
-//    public CustomResponseEntity<?> add(AuthorDto authorDto) throws SQLException {
-//        return null;
-//    }
-
-//    @Override public boolean add(Author author) throws SQLException {
-//
-//        return authorStatementExecutor.add(author);
-//    }
-
-    @Override public boolean delete(int id) throws SQLException {
+    @Override
+    public boolean delete(int id) throws SQLException {
 
         return authorStatementExecutor.delete(id);
     }
-    @Override public boolean update(Author author) throws SQLException {
+    @Override
+    public boolean update(Author author) throws SQLException {
 
         return authorStatementExecutor.update(author);
+    }
+
+    private String wrapper(String param) {
+
+        return "'" + param + "'";
     }
 }
