11ae5763d1ae0964996484475bac22e64dafe59c
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-02-21 09:23:33 +0200

    resolved unresolved commit

--- src/main/java/com/softserve/library/app/controller/servlets/book/BookAdvancedSearchServlet.java
+++ src/main/java/com/softserve/library/app/controller/servlets/book/BookAdvancedSearchServlet.java
@@ -50,30 +50,13 @@ public class BookAdvancedSearchServlet extends HttpServlet {
         bookParametersDto.setYearPublishedTo(yearTo);
         bookParametersDto.setAvailable(isAvailable);
 
-<<<<<<< HEAD
-        ServiceFactory serviceFactory = ServiceFactoryImpl.getFactory();
-
         List<BookDto> books = new ArrayList<>();
-||||||| merged common ancestors
-        ServiceFactory serviceFactory = ServiceFactoryImpl.getFactory();
-
-        List<Book> books = new ArrayList<>();
-=======
         final ServiceFactory serviceFactory = ServiceFactoryImpl.getFactory();
-        List<Book> books = new ArrayList<>();
->>>>>>> aba3c743fd12c404c5cd021efb3ee3459b7d1b10
 
         try {
 
             books = serviceFactory.getBookService().getAllByParameters(bookParametersDto);
-<<<<<<< HEAD
             resp.setStatus(resp.SC_CREATED);
-        } catch (SQLException e) {
-||||||| merged common ancestors
-        } catch (SQLException e) {
-=======
->>>>>>> aba3c743fd12c404c5cd021efb3ee3459b7d1b10
-
         } catch (SQLException e) {
 
             final RequestDispatcher dispatcherError = this.getServletContext().getRequestDispatcher("/WEB-INF/view/errors/Error500.jsp");
@@ -81,7 +64,6 @@ public class BookAdvancedSearchServlet extends HttpServlet {
             return;
         }
 
-<<<<<<< HEAD
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String jsonList = gson.toJson(books);
 
@@ -90,14 +72,5 @@ public class BookAdvancedSearchServlet extends HttpServlet {
 
         final String redirect = req.getContextPath() + UrlPatterns.BOOK_LIST;
         resp.sendRedirect(redirect);
-||||||| merged common ancestors
-        System.out.println();
-
-        final RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/view/general/advancedBookSearch.jsp");
-        dispatcher.forward(req, resp);
-=======
-        final RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/view/general/advancedBookSearch.jsp");
-        dispatcher.forward(req, resp);
->>>>>>> aba3c743fd12c404c5cd021efb3ee3459b7d1b10
     }
 }
